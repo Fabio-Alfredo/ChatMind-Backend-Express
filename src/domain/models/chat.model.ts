7e8a4e30ab00ed8745b5f290eb6fdbf541c64b35
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-22 11:48:54 -0600

    add(chat.model): new model for representation chat :sparkles:

---
+++ src/domain/models/chat.model.ts
@@ -0,0 +1,28 @@
+import { Schema, model, mongo } from 'mongoose';
+import { Chat } from '../../interface/chat.interface';
+
+const chatSchema = new Schema<Chat>(
+    {
+        user_id: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+        bot_id: {
+            type: Schema.Types.ObjectId,
+            ref: 'Bot',
+            required: true,
+        },
+        name: {
+            type: String,
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+)
+
+const ChatModel = model<Chat>('Chat', chatSchema);
+
+export default ChatModel;
