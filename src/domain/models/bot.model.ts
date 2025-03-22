c95d63255d87872f6e9350451379be217e704ec9
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-22 12:03:08 -0600

    add(bot.model): add new model for representation bot :sparkles:

---
+++ src/domain/models/bot.model.ts
@@ -0,0 +1,38 @@
+import { Schema, model } from "mongoose";
+import { Bot } from "../../interface/bot.interface";
+
+const botSchema = new Schema<Bot>(
+    {
+        name: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            required: true,
+        },
+        model: {
+            type: String,
+            required: true,
+        },
+        apiURL: {
+            type: String,
+            required: true,
+        },
+        apiToken: {
+            type: String,
+            required: true,
+        },
+        active: {
+            type: Boolean,
+            default: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+)
+
+const BotModel = model<Bot>('Bot', botSchema);
+
+export default BotModel;
