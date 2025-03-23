967538d915add90f1ec9ec155fcd4503438495b7
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-22 22:31:08 -0600

    refactor(user.controller): usage new method for catch errors :sparkles:

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -1,15 +1,24 @@
 import { Request, Response, NextFunction } from 'express';
 import * as userService from "../services/user.service";
 import { User } from "../interfaces/user.interface";
+import ErrorCodes from '../utils/error/codes/error.codes';
+import createHttpError from 'http-errors';
 
 
-export const createUser = async (req: Request<{},{}, User>, res: Response, next:NextFunction):Promise<any> => {
+export const createUser = async (req: Request<{}, {}, User>, res: Response, next: NextFunction): Promise<any> => {
     try {
-        const user:User = await userService.create(req.body);
+        const user: User = await userService.create(req.body);
         return res.status(201).json(user);
-    } catch (error) {
-        if (error instanceof Error)
-            return res.status(400).json({ message: error.message });
-        return res.status(500).json({ message: "Internal Server Error" });
+    } catch (e: any) {
+        switch (e.code) {
+            case ErrorCodes.USER.ALREADY_EXISTS:
+                next(createHttpError(409, e.message));
+                break;
+            case ErrorCodes.SERVER.INTERNAL_SERVER_ERROR:
+                next(createHttpError(500, e.message));
+                break;
+            default:
+                next(e);
+        }
     }
 }
