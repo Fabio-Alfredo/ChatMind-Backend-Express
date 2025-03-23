10c8d97bce58e933f1e544502b6330d478159587
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-22 18:23:13 -0600

    add: usage connection with mongoo

--- server.ts
+++ server.ts
@@ -1,5 +1,8 @@
 import { currentEnv } from "./src/config/config";
+import { connectDb } from "./src/config/db.config/mongo.config";
 import app from "./app";
+
+connectDb();
 
 app.listen(currentEnv.port, () => {
     console.log(`Server is running on http://localhost:${currentEnv.port}`);
