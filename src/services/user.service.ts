ce0f12a2c0078f5d080f0bb88fbb0b872e874be7
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-22 22:27:59 -0600

    add(user.service): usage service error for catch errors :sparkles:

--- src/services/user.service.ts
+++ src/services/user.service.ts
@@ -1,20 +1,24 @@
 import * as userRepo from "../repositories/user.repository";
 import { User } from "../interfaces/user.interface";
+import ServiceError from "../utils/error/service.error";
+import ErrorCodes from "../utils/error/codes/error.codes";
+
 
 export const create = async (user: User): Promise<User> => {
     try {
         const userExist = await userRepo.findByEmail(user.email);
         if (userExist) {
-            throw new Error("User already exists");
+            throw new ServiceError("User already exists",
+                ErrorCodes.USER.ALREADY_EXISTS
+            );
         }
 
         const newUser = await userRepo.create(user);
         return newUser;
-    } catch (error) {
-        if (error instanceof Error) {
-            throw new Error(error.message);
-        }
-        throw new Error("An error occurred while creating the user");
+    } catch (e: any) {
+        throw new ServiceError(e.message || "Internal Server Error",
+            e.code || ErrorCodes.SERVER.INTERNAL_SERVER_ERROR
+        );
     }
 }
 
