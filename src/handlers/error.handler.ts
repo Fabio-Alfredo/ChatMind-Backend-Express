fbaf82bcb16e75d2913aa7de32a04788dab4b4e6
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-24 11:22:14 -0600

    add(error.handler): new method for handler errors :sparkles:

--- src/handlers/error.handler.ts
+++ src/handlers/error.handler.ts
@@ -1,10 +1,10 @@
 import { Request, Response, NextFunction } from "express"
 
 interface CustomError extends Error {
-    status?: number;
+    status: number;
 }
 
 export const errorHander = (err: CustomError, req: Request, res: Response, next: NextFunction):void=> {
-    console.error(err);
+    console.error(err.message);
     res.status(err.status || 500).json({ message: err.message || "Internal Server Error" });
 }
