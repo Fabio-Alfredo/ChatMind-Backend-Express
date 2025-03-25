d5cfb973de20de096183826aff5c6df022378b8e
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-25 10:14:52 -0600

    bug(user.interface): solution bug for exportation type :bug:

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -11,4 +11,4 @@ type authUser = Pick<User, "email" | "password">;
 type registerUser = Pick<User, "name" | "email" | "password">;
 type tokenPayload = Pick<User, "_id" | "roles">;
 
-export { User, authUser, registerUser };
+export { User, authUser, registerUser, tokenPayload };
