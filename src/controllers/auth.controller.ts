516339472a6c9da4e3ba026020617d18951b079e
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-25 17:39:33 -0600

    add(auth.controller):new controller for login one user :sparkles:

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import * as userService from "../services/auth.service";
-import { registerUser } from "../interfaces/user.interface";
+import { registerUser, authUser } from "../interfaces/user.interface";
 import ErrorCodes from '../utils/error/codes/error.codes';
 import createHttpError from 'http-errors';
 import { responseHandler } from '../handlers/response.handler';
@@ -16,6 +16,24 @@ export const createUser = async (req: Request<{}, {}, registerUser>, res: Respon
         switch (e.code) {
             case ErrorCodes.USER.ALREADY_EXISTS:
                 next(createHttpError(409, e.message));
+                break;
+            case ErrorCodes.SERVER.INTERNAL_SERVER_ERROR:
+                next(createHttpError(500, e.message));
+                break;
+            default:
+                next(e);
+        }
+    }
+}
+
+export const loginUser = async (req: Request<{}, {}, authUser>, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        const token = await userService.auth(req.body);
+        return responseHandler(res, "User authenticated", 200, { token });
+    } catch (e: any) {
+        switch (e.code) {
+            case ErrorCodes.USER.INVALID_CREDENTIALS:
+                next(createHttpError(401, e.message));
                 break;
             case ErrorCodes.SERVER.INTERNAL_SERVER_ERROR:
                 next(createHttpError(500, e.message));
