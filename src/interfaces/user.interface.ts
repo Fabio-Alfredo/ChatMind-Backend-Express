15c89b742cab94ec6367701766d4496cb1c0e9da
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-25 10:13:35 -0600

    add(user.interface): new type for data in token :sparkles:

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -9,5 +9,6 @@ interface User {
 
 type authUser = Pick<User, "email" | "password">;
 type registerUser = Pick<User, "name" | "email" | "password">;
+type tokenPayload = Pick<User, "_id" | "roles">;
 
 export { User, authUser, registerUser };
