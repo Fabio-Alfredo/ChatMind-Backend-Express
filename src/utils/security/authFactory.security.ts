1be2af7af73147bf40ee9674d3024704dff48986
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-26 10:27:47 -0600

    bug(authFactory):solution bug in select type auth :bug:

--- src/utils/security/authFactory.security.ts
+++ src/utils/security/authFactory.security.ts
@@ -1,15 +1,20 @@
 import { JWT_STRATEGY } from "./jwt.security"
 import { authTypes } from "../../types/jwt.type";
+import ServiceError from "../error/service.error";
+import ErrorCodes from "../error/codes/error.codes";
 
 const AuthFactory = {
     AuthProvider: (option: authTypes) => {
         switch (option) {
             case "JWT":
                 return JWT_STRATEGY;
-            case "OAUTH":
-                return
+            //TODO: implementation of OAUTH
+            // case "OAUTH":
+            //     return
             default:
-                return
+                throw new ServiceError("Invalid Auth Provider",
+                    ErrorCodes.SERVER.INTERNAL_SERVER_ERROR
+                );
         }
     }
 }
