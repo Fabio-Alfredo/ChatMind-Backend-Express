485d756953facd9d5d2f3997f63b6e0ffbbfc4ef
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-26 10:56:25 -0600

    refactor(jwt.security): usage new interface by token :sparkles:

--- src/utils/security/jwt.security.ts
+++ src/utils/security/jwt.security.ts
@@ -1,11 +1,16 @@
 import { sign, verify } from 'jsonwebtoken';
 import { tokenPayload } from '../../interfaces/user.interface';
+import { Token } from '../../interfaces/toke.interface';
 import { currentEnv } from '../../configs/config';
 
 const JWT_STRATEGY = {
-    generateToken: (payload: tokenPayload): string => {
+    generateToken: (payload: tokenPayload): Token => {
         const token = sign(payload, currentEnv.jwtSecret as string, { expiresIn: "1h" });
-        return token;
+        return {
+            token,
+            expiresIn: 3600,
+            type: "Bearer"
+        };
     },
     verifyToken: (token: string): tokenPayload => {
         const payload = verify(token, currentEnv.jwtSecret as string) as tokenPayload;
