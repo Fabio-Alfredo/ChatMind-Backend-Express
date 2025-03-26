35812b50440a38b4088de5840d2ed77d0cf4b65c
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-26 11:17:12 -0600

    add(index): new index for export all interfaces :sparkles:

---
+++ src/interfaces/index.ts
@@ -0,0 +1,7 @@
+export * from "./user.interface";
+export * from "./toke.interface";
+export * from "./message.interface";
+export * from "./config.interface";
+export * from "./chat.interface";
+export * from "./bot.interface";
+
