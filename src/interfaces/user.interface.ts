5d89fb06645dc2547d889ecbf87cf90336ba1b44
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-27 14:51:31 -0600

    add(user.interface): new type for register user by google :sparkles:

--- src/interfaces/user.interface.ts
+++ src/interfaces/user.interface.ts
@@ -10,8 +10,9 @@ interface User {
 }
 
 type AuthUser = Pick<User, "email" | "password">;
-type RegisterUser = Pick<User, "name" | "email" | "password">;
+type RegisterUser = Pick<User, "name" | "email" | "password" >;
+type RegisterGoogleUser = Pick<User, "name" | "email" | "authProvider">;
 type TokenPayload = Pick<User, "_id" | "roles">;
 type GooglePayload = Pick<User, "name" | "email">;
 
-export { User, AuthUser, RegisterUser, TokenPayload, GooglePayload };
+export { User, AuthUser, RegisterUser, TokenPayload, GooglePayload, RegisterGoogleUser };
