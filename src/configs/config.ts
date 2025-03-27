abd5239e4f00bf7a78369597676c5b8dd967e0fa
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-27 10:55:09 -0600

    add(config): google id configuration :sparkles:

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -2,26 +2,29 @@ import "dotenv/config";
 import { Config } from "../interfaces";
 import { Env } from "../types/env.type";
 
-const { PORT, NODE_ENV, MONGO_URI, JWT_SECRET } = process.env;
+const { PORT, NODE_ENV, MONGO_URI, JWT_SECRET, GOOGLE_CLIENT_ID } = process.env;
 
-const config:Record<Env, Config> = {
+const config: Record<Env, Config> = {
     development: {
         port: parseInt(PORT as string) || 3000,
-        nodeEnv: (NODE_ENV ) || "development",
-        mongoUri: MONGO_URI ,
+        nodeEnv: (NODE_ENV) || "development",
+        mongoUri: MONGO_URI,
         jwtSecret: JWT_SECRET,
+        googleClientId: GOOGLE_CLIENT_ID
     },
     production: {
         port: parseInt(PORT as string) || 3000,
         nodeEnv: (NODE_ENV as Env) || "production",
         mongoUri: MONGO_URI as string,
         jwtSecret: JWT_SECRET,
+        googleClientId: GOOGLE_CLIENT_ID
     },
     test: {
         port: parseInt(PORT as string) || 3000,
         nodeEnv: (NODE_ENV as Env) || "test",
         mongoUri: MONGO_URI as string,
         jwtSecret: JWT_SECRET,
+        googleClientId: GOOGLE_CLIENT_ID
     },
 }
 
