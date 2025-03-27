e081194311589bc488f6d9bfba5ed327d0f0bc94
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-27 14:35:06 -0600

    add(auth.controller): new controller for auth user by google :sparkles:

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -1,6 +1,6 @@
 import { Request, Response, NextFunction } from 'express';
 import * as userService from "../services/auth.service";
-import {RegisterUser, AuthUser, Token} from "../interfaces";
+import { RegisterUser, AuthUser, Token } from "../interfaces";
 import ErrorCodes from '../utils/error/codes/error.codes';
 import createHttpError from 'http-errors';
 import { responseHandler } from '../handlers/response.handler';
@@ -32,6 +32,25 @@ export const loginUser = async (req: Request<{}, {}, AuthUser>, res: Response, n
     } catch (e: any) {
         switch (e.code) {
             case ErrorCodes.USER.INVALID_CREDENTIALS:
+                next(createHttpError(401, e.message));
+                break;
+            case ErrorCodes.SERVER.INTERNAL_SERVER_ERROR:
+                next(createHttpError(500, e.message));
+                break;
+            default:
+                next(e);
+        }
+    }
+}
+
+export const googleAuth = async (req: Request<{}, {}, { googleToken: string }>, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        const tokenGoogle = req.body.googleToken;
+        const token: Token = await userService.googleAuth(tokenGoogle);
+        return responseHandler(res, "User authenticated with Google", 200, token);
+    } catch (e: any) {
+        switch (e.code) {
+            case ErrorCodes.USER.INVALID_GOOGLE_TOKEN:
                 next(createHttpError(401, e.message));
                 break;
             case ErrorCodes.SERVER.INTERNAL_SERVER_ERROR:
