31f3da855a508fa1a439abbffb57e7317aea5c99
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-27 12:14:51 -0600

    refactor(jwt.security): new implementation for methdo by auth user :pencil2:

--- src/utils/security/jwt.security.ts
+++ src/utils/security/jwt.security.ts
@@ -3,36 +3,27 @@ import { TokenPayload, Token, GooglePayload } from "../../interfaces";
 import { currentEnv } from '../../configs/config';
 import client from '../../configs/google/googleAuth.config';
 
-const JWT_STRATEGY = {
-    generateToken: (payload: TokenPayload): Token => {
-        const token = sign(payload, currentEnv.jwtSecret as string, { expiresIn: "1h" });
-        return {
-            token,
-            expiresIn: 3600,
-            type: "Bearer"
-        };
-    },
-    verifyToken: (token: string): TokenPayload => {
-        const payload = verify(token, currentEnv.jwtSecret as string) as TokenPayload;
-        return payload;
-    }
-};
 
-const OAUTH_STRATEGY = {
-    verifyToken: async (token: string): Promise<GooglePayload> => {
-        const ticket = await client.verifyIdToken({
-            idToken: token,
-            audience: currentEnv.googleClientId
-        });
-
-        const payload = ticket.getPayload();
-        return payload as GooglePayload;
-    },
-
-    generateToken: (): never => {
-        throw new Error("OAuth does not support token generation.");
-    }
-
+export const generateToken = (payload: TokenPayload): Token => {
+    const token = sign(payload, currentEnv.jwtSecret as string, { expiresIn: "1h" });
+    return {
+        token,
+        expiresIn: 3600,
+        type: "Bearer"
+    };
+}
+export const verifyToken = (token: string): TokenPayload => {
+    const payload = verify(token, currentEnv.jwtSecret as string) as TokenPayload;
+    return payload;
 }
 
-export { JWT_STRATEGY, OAUTH_STRATEGY };
+export const verifyGoogleAuth = async (token: string): Promise<GooglePayload> => {
+    const ticket = await client.verifyIdToken({
+        idToken: token,
+        audience: currentEnv.googleClientId
+    });
+
+    const payload = ticket.getPayload();
+    return payload as GooglePayload;
+}
+
