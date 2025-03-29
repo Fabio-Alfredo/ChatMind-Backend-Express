d60a6b153a01e813b73c8ed382f68ec171653cc9
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-29 15:05:44 -0600

    add(bot.interface): new type for create bot

--- src/interfaces/bot.interface.ts
+++ src/interfaces/bot.interface.ts
@@ -1,12 +1,14 @@
 import mongoose from "mongoose";
 
 interface Bot {
+    _id: mongoose.Schema.Types.ObjectId;
     name: string;
     description: string;
     model: string;
     apiURL: string;
-    apiToken: string;
+    apiToken?: string;
     active: boolean;
 }
 
-export { Bot };
+type CreateBot = Pick<Bot, "name" | "description" | "model" | "apiURL" >;
+export { Bot, CreateBot };
