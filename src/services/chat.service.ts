321eece78d12d3a315005a6f73db7b8c56cae1f2
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-29 14:24:28 -0600

    add(chat.service): new service for find chat by id :sparkle:

--- src/services/chat.service.ts
+++ src/services/chat.service.ts
@@ -2,6 +2,7 @@ import * as chatRepo from "../repositories/chat.repository";
 import ServiceError from "../utils/error/service.error";
 import ErrorCodes from "../utils/error/codes/error.codes";
 import { Chat, CreateChat } from "../interfaces";
+import { Schema } from "mongoose";
 
 export const creteChat = async (chat: CreateChat): Promise<Chat> => {
     try {
@@ -12,5 +13,22 @@ export const creteChat = async (chat: CreateChat): Promise<Chat> => {
         throw new ServiceError(e.message || "Internal Server Error",
             e.code || ErrorCodes.SERVER.INTERNAL_SERVER_ERROR
         );
+    }
+}
+
+export const findChatById = async (id: Schema.Types.ObjectId): Promise<Chat> => {
+    try {
+        const chat = await chatRepo.findById(id);
+        if (!chat) {
+            throw new ServiceError("Chat not found",
+                ErrorCodes.CHAT.NOT_FOUND
+            );
+        }
+
+        return chat;
+    } catch (e: any) {
+        throw new ServiceError(e.message || "Internal Server Error",
+            e.code || ErrorCodes.SERVER.INTERNAL_SERVER_ERROR
+        )
     }
 }
