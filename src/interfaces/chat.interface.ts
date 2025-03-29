0bed5950e74fe406fa241ea310fb1686ec3f0be2
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-29 14:08:33 -0600

    add(chat.interface): new type for chat

--- src/interfaces/chat.interface.ts
+++ src/interfaces/chat.interface.ts
@@ -1,9 +1,10 @@
 import mongoose from "mongoose";
 
 interface Chat {
+    _id: mongoose.Schema.Types.ObjectId;
     user_id: mongoose.Schema.Types.ObjectId;
     bot_id: mongoose.Schema.Types.ObjectId;
     name: string;
 }
-
-export { Chat };
+type creteChat = Pick<Chat, "user_id" | "bot_id" | "name">;
+export { Chat, creteChat };
