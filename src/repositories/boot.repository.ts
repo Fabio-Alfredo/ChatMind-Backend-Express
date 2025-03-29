4fc5cb79494552fa69bd0215fd5db8aaf67ed5fa
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-29 15:08:35 -0600

    add(boot.repository): new method for find one bot :sparkles:

--- src/repositories/boot.repository.ts
+++ src/repositories/boot.repository.ts
@@ -1,7 +1,13 @@
 import BotModel from "../domains/models/bot.model";
 import { Bot, CreateBot } from "../interfaces";
+import { Schema } from 'mongoose';
 
 export const create = async (bot: CreateBot): Promise<Bot> => {
     const newBot = await BotModel.create(bot);
     return newBot;
+}
+
+export const findById = async (id: Schema.Types.ObjectId): Promise<Bot | null> => {
+    const bot = await BotModel.findById(id);
+    return bot;
 }
