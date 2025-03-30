0b1d192885ec2af44774cf343096f154b6acaa13
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-30 10:47:16 -0600

    add(bot.route): usage new validations for create bot :sparkles:

--- src/routes/bot.route.ts
+++ src/routes/bot.route.ts
@@ -1,9 +1,16 @@
 import * as botController from '../controllers/bot.controller';
 import { Router } from 'express';
+import { createValidator } from '../validators/bot.validator';
+import validatorMiddleware from '../middleware/validator.middleware';
 
 const router = Router();
 
-router.post('/create', botController.createBot);
+router.post(
+    '/create',
+    createValidator,
+    validatorMiddleware,
+    botController.createBot
+);
 router.get('/:id', botController.findBotById);
 
 export default router;
