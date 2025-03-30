c8a1be79bd5e576c3ed78fdd0067f480935f5d70
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-30 10:41:47 -0600

    add(bot.service): new validation for bot already exista :sparkles:

--- src/services/bot.service.ts
+++ src/services/bot.service.ts
@@ -6,8 +6,13 @@ import { Schema } from "mongoose";
 
 export const create = async (bot: CreateBot): Promise<Bot> => {
     try {
-        //TODO: Check if the bot already exists
-        const newBot = await BotRepo.create(bot);
+        const existBot: boolean = await BotRepo.existBot(bot.name, bot.apiURL);
+        if (existBot) {
+            throw new ServiceError("There is already a bot with that name or URL", ErrorCodes.BOT.ALREADY_EXISTS_BOT)
+        }
+
+        const newBot: Bot = await BotRepo.create(bot);
+
         return newBot;
     } catch (e: any) {
         throw new ServiceError(e.message || "Internal Server Error",
@@ -18,13 +23,12 @@ export const create = async (bot: CreateBot): Promise<Bot> => {
 
 export const findBotById = async (id: Schema.Types.ObjectId): Promise<Bot> => {
     try {
-        const bot = await BotRepo.findById(id);
+        const bot: Bot | null = await BotRepo.findById(id);
         if (!bot) {
             throw new ServiceError("Bot not found",
                 ErrorCodes.BOT.NOT_FOUND
             );
         }
-
         return bot;
     } catch (e: any) {
         throw new ServiceError(e.message || "Internal Server Error",
