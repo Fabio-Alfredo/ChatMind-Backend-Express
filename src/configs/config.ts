adc0838731405eb61b662a88ef3918b4ea90c3cf
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-30 09:51:53 -0600

    add(config): new variable for encrypt :sparkles:

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -2,7 +2,7 @@ import "dotenv/config";
 import { Config } from "../interfaces";
 import { Env } from "../types/env.type";
 
-const { PORT, NODE_ENV, MONGO_URI, JWT_SECRET, GOOGLE_CLIENT_ID } = process.env;
+const { PORT, NODE_ENV, MONGO_URI, JWT_SECRET, GOOGLE_CLIENT_ID, SECRET_CRYPT_KEY } = process.env;
 
 const config: Record<Env, Config> = {
     development: {
@@ -10,21 +10,24 @@ const config: Record<Env, Config> = {
         nodeEnv: (NODE_ENV) || "development",
         mongoUri: MONGO_URI,
         jwtSecret: JWT_SECRET,
-        googleClientId: GOOGLE_CLIENT_ID
+        googleClientId: GOOGLE_CLIENT_ID,
+        encryptKey: SECRET_CRYPT_KEY || "defaultEncryptKey"
     },
     production: {
         port: parseInt(PORT as string) || 3000,
         nodeEnv: (NODE_ENV as Env) || "production",
         mongoUri: MONGO_URI as string,
         jwtSecret: JWT_SECRET,
-        googleClientId: GOOGLE_CLIENT_ID
+        googleClientId: GOOGLE_CLIENT_ID,
+        encryptKey: SECRET_CRYPT_KEY || "defaultEncryptKey"
     },
     test: {
         port: parseInt(PORT as string) || 3000,
         nodeEnv: (NODE_ENV as Env) || "test",
         mongoUri: MONGO_URI as string,
         jwtSecret: JWT_SECRET,
-        googleClientId: GOOGLE_CLIENT_ID
+        googleClientId: GOOGLE_CLIENT_ID,
+        encryptKey: SECRET_CRYPT_KEY || "defaultEncryptKey"
     },
 }
 
