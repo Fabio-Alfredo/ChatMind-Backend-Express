8aeb630da2431831512b2752428f366131dde3ee
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-31 11:06:24 -0600

    add(token.interface): new interface for valdit information token :sparkles:

--- src/interfaces/toke.interface.ts
+++ src/interfaces/toke.interface.ts
@@ -1,8 +1,15 @@
+import { TokenPayload } from "./user.interface";
+
 interface Token {
     token: string;
     expiresIn: number;
     type: string;
 }
 
+interface TokenValid {
+    valid: boolean;
+    message?: string;
+    payload?: TokenPayload;
+}
 
-export { Token };
+export { Token, TokenValid };
