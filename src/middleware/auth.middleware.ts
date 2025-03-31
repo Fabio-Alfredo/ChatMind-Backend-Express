3d5cd5ef5fa647040f5328d49422b272ff6b290a
Fabio-Alfredo <00210121@uca.edu.sv>
2025-03-31 12:09:33 -0600

    bug(auth.middleware): solution bug in insert data for req :sparkles:

--- src/middleware/auth.middleware.ts
+++ src/middleware/auth.middleware.ts
@@ -1,23 +1,28 @@
 import { Request, Response, NextFunction } from "express";
 import createHttpError from "http-errors";
 import { verifyToken } from "../utils/security/jwt.security";
-import { AuthRequest } from "../interfaces";
+import { TokenPayload } from "../interfaces";
 
+declare module "express-serve-static-core" {
+    interface Request {
+        dataUser: TokenPayload;
+    }
+}
 
-export const authMiddleware = (req:AuthRequest, res:Response, next:NextFunction):void =>{
-    try{
+export const authMiddleware = (req: Request, res: Response, next: NextFunction): void => {
+    try {
         const token: string | null = req.headers["authorization"]?.split(" ")[1] || null;
 
-        if(!token) return next(createHttpError(401, "Unauthorized"));
+        if (!token) return next(createHttpError(401, "Unauthorized"));
 
-        const {valid, message, payload} = verifyToken(token);
+        const { valid, message, payload } = verifyToken(token);
 
-        if(!valid || !payload) return next(createHttpError(401, message || "Unauthorized"));
-
-        req.user = payload;
+        if (!valid || !payload) return next(createHttpError(401, message || "Unauthorized"));
+        req.dataUser = payload;
+        // req.token = token;
 
         next();
-    }catch(err){
+    } catch (err) {
         console.error(err);
         next(createHttpError(401, "Unauthorized"));
     }
