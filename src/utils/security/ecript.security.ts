55456ae2a41c23cca043b6b07ec47a125db0a5a6
Fabio-Alfredo <00210121@uca.edu.sv>
2025-04-02 12:11:37 -0600

    bug(encript.sercurity): solution bug in function for create encrypt :bug:

--- src/utils/security/ecript.security.ts
+++ src/utils/security/ecript.security.ts
@@ -3,29 +3,26 @@ import { currentEnv } from "../../configs/config";
 
 export const generateHash = (plainText: string): string => {
     //crea un vector de 16 bytes aleatorio
-    const iv = cripto.randomBytes(16).toString("hex");
-    //crea un objeto de cifrado con el algoritmo aes-256-ocb
-    const cipher = cripto.createCipheriv("aes-256-ocb", currentEnv.encryptKey, iv, { authTagLength: 16 });
+    const iv = cripto.randomBytes(16);
+    //crea un objeto de cifrado con el algoritmo aes-256-ocb    
+    const cipher = cripto.createCipheriv("aes-256-cbc", currentEnv.encryptKey, iv);
     //cifra el texto plano y lo convierte a hex
     let cipherText = cipher.update(plainText, "utf8", "hex");
     //finaliza el cifrado y lo convierte a hex
     cipherText += cipher.final("hex");
-    //obtiene la etiqueta de autenticación y la convierte a hex
-    const tag = cipher.getAuthTag().toString("hex");
     //retorna el iv, el texto cifrado y la etiqueta de autenticación separados por :
-    return `${iv}:${cipherText}:${tag}`;
+    return iv.toString() + cipherText.toString();
 };
 
 export const compareHash = (cryptText: string): string => {
-    //separa el texto cifrado en iv, texto cifrado y etiqueta de autenticación
-    const decipher = cripto.createDecipheriv("aes-256-ocb", currentEnv.encryptKey, cryptText.split(":")[0], { authTagLength: 16 });
-    //decifra el texto cifrado y lo convierte a utf8
-    decipher.setAuthTag(Buffer.from(cryptText.split(":")[2], "hex"));
-    //decifra el texto cifrado y lo convierte a utf8
-    let decryptedText = decipher.update(cryptText.split(":")[1], "hex", "utf8");
-    //finaliza el cifrado y lo convierte a utf8
-    decryptedText += decipher.final("utf8");
-    //retorna el texto plano
-    return decryptedText;
+    //separa el iv, el texto cifrado y la etiqueta de autenticación
+    const [iv, cipherText] = cryptText.split(":");
+    //crea un objeto de descifrado con el algoritmo aes-256-ocb
+    const decipher = cripto.createDecipheriv("aes-256-ocb", currentEnv.encryptKey, Buffer.from(iv, "hex"));
+    //descifra el texto cifrado y lo convierte a utf8
+    let plainText = decipher.update(cipherText, "hex", "utf8");
+    //finaliza el descifrado y lo convierte a utf8
+    plainText += decipher.final("utf8");
+    return plainText;
 };
 
