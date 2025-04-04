62049e738204828239ee022a29df4465da962617
Fabio-Alfredo <00210121@uca.edu.sv>
2025-04-04 11:21:14 -0600

    add(bot.repository): new functions for update find and delete bots :sparkles:

--- src/repositories/boot.repository.ts
+++ src/repositories/boot.repository.ts
@@ -2,9 +2,9 @@ import BotModel from "../domains/models/bot.model";
 import { Bot, CreateBot } from "../interfaces";
 import { Schema } from 'mongoose';
 
-export const create = async (bot: CreateBot, createBy:Schema.Types.ObjectId): Promise<Bot> => {
+export const create = async (bot: CreateBot, createBy: Schema.Types.ObjectId): Promise<Bot> => {
     console.log("aca esta el peo", bot)
-    const newBot = await BotModel.create({createBy, ...bot});
+    const newBot = await BotModel.create({ createBy, ...bot });
     return newBot;
 }
 
@@ -16,4 +16,19 @@ export const findById = async (id: Schema.Types.ObjectId): Promise<Bot | null> =
 export const existBot = async (name: string, url: string): Promise<boolean> => {
     const bot = await BotModel.findOne({ name, apiURL: url })
     return bot ? true : false;
+}
+
+export const updateBot = async (id: Schema.Types.ObjectId, bot: CreateBot): Promise<Bot | null> => {
+    const updateBot = await BotModel.findOneAndUpdate({ _id: id }, { $set: bot }, { new: true });
+    return updateBot;
+}
+
+export const findAll = async (): Promise<Bot[]> => {
+    const bots = await BotModel.find().populate("createBy", "name email");
+    return bots;
+}
+
+export const desactivateBot = async (id: Schema.Types.ObjectId): Promise<Bot | null> => {
+    const bot = await BotModel.findByIdAndUpdate(id, { active: false }, { new: true });
+    return bot;
 }
