7614b3b4e1274412b3227e1e5e341a17bcda51dc
Fabio-Alfredo <00210121@uca.edu.sv>
2025-04-05 19:05:13 -0600

    add(bot.controller): add new controller for update and find bots :sparkles:

--- src/controllers/bot.controller.ts
+++ src/controllers/bot.controller.ts
@@ -1,7 +1,7 @@
 import * as botService from "../services/bot.service";
 import createHttpError from "http-errors";
 import { Request, Response, NextFunction } from "express";
-import { Bot, CreateBot, TokenPayload } from "../interfaces";
+import { Bot, CreateBot, TokenPayload, UpdateBot } from "../interfaces";
 import { Schema } from "mongoose";
 import { responseHandler } from "../handlers/response.handler";
 
@@ -9,7 +9,7 @@ export const createBot = async (req: Request<{}, {}, CreateBot>, res: Response, 
 
     try {
         const bot: CreateBot = req.body;
-        const user:TokenPayload = req.dataUser;
+        const user: TokenPayload = req.dataUser;
         const newBot: Bot = await botService.create(bot, user);
         return responseHandler(res, "created bot", 201, newBot);
     } catch (e: any) {
@@ -35,6 +35,63 @@ export const findBotById = async (req: Request<{ id: Schema.Types.ObjectId }>, r
         const botId: Schema.Types.ObjectId = req.params.id;
         const bot: Bot = await botService.findBotById(botId);
         return responseHandler(res, "found bot", 200, bot);
+    } catch (e: any) {
+        switch (e.code) {
+            case "BOT_NOT_FOUND":
+                next(createHttpError(404, e.message));
+                break;
+            case "INTERNAL_SERVER_ERROR":
+                next(createHttpError(500, e.message));
+                break;
+            default:
+                next(e);
+        }
+    }
+}
+
+export const findAll = async (req: Request, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        const bots: Bot[] = await botService.findAll();
+        return responseHandler(res, "found bots", 200, bots);
+    } catch (e: any) {
+        switch (e.code) {
+            case "BOT_NOT_FOUND":
+                next(createHttpError(404, e.message));
+                break;
+            case "INTERNAL_SERVER_ERROR":
+                next(createHttpError(500, e.message));
+                break;
+            default:
+                next(e);
+        }
+    }
+}
+
+export const updateBot = async (req: Request<{ id: Schema.Types.ObjectId }, {}, UpdateBot>, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        const botId: Schema.Types.ObjectId = req.params.id;
+        const bot: UpdateBot = req.body;
+        const updatedBot: Bot = await botService.updateBot(botId, bot);
+        return responseHandler(res, "updated bot", 200, updatedBot);
+    } catch (e: any) {
+        switch (e.code) {
+            case "BOT_NOT_FOUND":
+                next(createHttpError(404, e.message));
+                break;
+            case "INTERNAL_SERVER_ERROR":
+                next(createHttpError(500, e.message));
+                break;
+            default:
+                next(e);
+        }
+    }
+}
+
+export const desactivateBot = async (req: Request<{ id: Schema.Types.ObjectId }, {}, {}>, res: Response, next: NextFunction): Promise<void> => {
+    try {
+        const botId: Schema.Types.ObjectId = req.params.id;
+        await botService.desactivateBot(botId);
+        return responseHandler(res, "desactivated bot", 200, {});
     } catch (e: any) {
         switch (e.code) {
             case "BOT_NOT_FOUND":
