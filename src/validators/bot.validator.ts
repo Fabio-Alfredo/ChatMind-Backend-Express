b30ea321848102c951a9f5be16c238d0efdc2f28
Fabio-Alfredo <00210121@uca.edu.sv>
2025-04-05 19:20:35 -0600

    add(bot.validator): new validator for update and validate id in request :sparkles:

--- src/validators/bot.validator.ts
+++ src/validators/bot.validator.ts
@@ -1,4 +1,4 @@
-import { body } from "express-validator";
+import { body, param } from "express-validator";
 
 export const createValidator = [
     body("name")
@@ -40,5 +40,51 @@ export const createValidator = [
         .withMessage("API Token must be a string")
         .isLength({ min: 3 })
         .withMessage("API Token must be at least 3 characters long")
+        .bail(),
+]
+
+export const updateValidator = [
+    body("name")
+        .optional()
+        .isString()
+        .withMessage("Name must be a string")
+        .isLength({ min: 3 })
+        .withMessage("Name must be at least 3 characters long")
+        .bail(),
+    body("description")
+        .optional()
+        .isString()
+        .withMessage("Description must be a string")
+        .isLength({ min: 3 })
+        .withMessage("Description must be at least 3 characters long")
+        .bail(),
+    body("model")
+        .optional()
+        .isString()
+        .withMessage("Model must be a string")
+        .isLength({ min: 3 })
+        .withMessage("Model must be at least 3 characters long")
+        .bail(),
+    body("apiURL")
+        .optional()
+        .isString()
+        .withMessage("API URL must be a string")
+        .isLength({ min: 3 })
+        .withMessage("API URL must be at least 3 characters long")
+        .bail(),
+    body("apiToken")
+        .optional()
+        .isString()
+        .withMessage("API Token must be a string")
+        .isLength({ min: 3 })
+        .withMessage("API Token must be at least 3 characters long")
+]
+
+export const findByIdValidator = [
+    param("id")
+        .exists()
+        .withMessage("ID is required")
+        .isMongoId()
+        .withMessage("ID must be a valid MongoDB ObjectId")
         .bail(),
 ]
