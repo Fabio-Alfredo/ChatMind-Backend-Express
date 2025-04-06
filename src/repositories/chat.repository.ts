d5b20e53664f028dbb55022603f2bc5e260627c1
Fabio-Alfredo <00210121@uca.edu.sv>
2025-04-05 21:10:40 -0600

    bug(chat.repository): solution bug in types :bug:

--- src/repositories/chat.repository.ts
+++ src/repositories/chat.repository.ts
@@ -7,12 +7,12 @@ export const create = async (chat: CreateChat): Promise<Chat> => {
     return newChat;
 }
 
-export const findById = async (id: Schema.Types.ObjectId): Promise<Chat | null> => {
+export const findById = async (id: string): Promise<Chat | null> => {
     const chat = await ChatModel.findById(id);
     return chat;
 }
 
-export const findAllByUserId = async (userId: Schema.Types.ObjectId): Promise<Chat[]> => {
+export const findAllByUserId = async (userId: string): Promise<Chat[]> => {
     const chats = await ChatModel.find({ user_id: userId });
     return chats;
 }
