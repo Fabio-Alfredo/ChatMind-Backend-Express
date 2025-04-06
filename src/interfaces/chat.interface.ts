5b4c0ffeb69a6e24a9342b395c0b6e40563631a6
Fabio-Alfredo <00210121@uca.edu.sv>
2025-04-05 21:38:06 -0600

    add(chat.interface): new param in chat type for include messages :sparkles:

--- src/interfaces/chat.interface.ts
+++ src/interfaces/chat.interface.ts
@@ -5,6 +5,7 @@ interface Chat {
     user_id: mongoose.Schema.Types.ObjectId;
     bot_id: mongoose.Schema.Types.ObjectId;
     name: string;
+    messages: mongoose.Schema.Types.ObjectId[];
 }
 type CreateChat = Pick<Chat, "user_id" | "bot_id" | "name">;
 export { Chat, CreateChat };
