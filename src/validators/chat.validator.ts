90b3442b382ca8d0586e5fc11af37ffd43fa93c6
Fabio-Alfredo <00210121@uca.edu.sv>
2025-04-06 11:07:56 -0600

    add(chat.validator): new validations for create update and find chat :sparkles:

---
+++ src/validators/chat.validator.ts
@@ -0,0 +1,41 @@
+import { body, param } from "express-validator";
+
+export const createValidator = [
+    body("bot_id")
+    .exists()
+    .trim()
+    .withMessage("bot_id is required")
+    .isMongoId()
+    .withMessage("bot_id must be a valid MongoDB ObjectId"),
+
+    body("name")
+    .exists()
+    .trim()
+    .withMessage("name is required")
+    .isString()
+    .withMessage("name must be a string")
+    .isLength({ min: 1 })
+    .withMessage("name must be at least 1 character long")
+    .bail()
+]
+
+export const updateValidator = [
+    body("name")
+    .optional()
+    .trim()
+    .isString()
+    .withMessage("name must be a string")
+    .isLength({ min: 1 })
+    .withMessage("name must be at least 1 character long")
+    .bail()
+]
+
+export const findByIdValidator = [
+    param("id")
+    .exists()
+    .trim()
+    .withMessage("id is required")
+    .isMongoId()
+    .withMessage("id must be a valid MongoDB ObjectId")
+    .bail()
+]
