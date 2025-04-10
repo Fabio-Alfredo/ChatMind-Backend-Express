8271e3587f4379e44ca0f7146da9336da101d95b
Fabio-Alfredo <00210121@uca.edu.sv>
2025-04-10 11:25:30 -0600

    add(message.service): add new service for findAll messages by chat :sparkles:

--- src/services/message.service.ts
+++ src/services/message.service.ts
@@ -4,12 +4,29 @@ import ErrorCodes from "../utils/error/codes/error.codes";
 import { CreateMessage, Message } from "../interfaces";
 import { Schema } from "mongoose";
 
-export const createMessage = async (message: CreateMessage, user_id:Schema.Types.ObjectId): Promise<Message> => {
+export const createMessage = async (message: CreateMessage, user_id: Schema.Types.ObjectId): Promise<Message> => {
     try {
         const newMessage: Message | null = await messageRepo.create(message, user_id);
         return newMessage;
     } catch (e: any) {
         throw new ServiceError(e.message || "Internal Server Error",
+            e.code || ErrorCodes.SERVER.INTERNAL_SERVER_ERROR
+        )
+    }
+}
+
+export const findAllByChatId = async (chatId: string): Promise<Message[]> => {
+    try {
+        const messages: Message[] = await messageRepo.findAllByChatId(chatId);
+        if (!messages) {
+            throw new ServiceError("Messages not found",
+                ErrorCodes.MESSAGE.NOT_FOUND
+            );
+        }
+        return messages;
+    } catch (e: any) {
+        throw new ServiceError(
+            e.message || "Internal Server Error",
             e.code || ErrorCodes.SERVER.INTERNAL_SERVER_ERROR
         )
     }
