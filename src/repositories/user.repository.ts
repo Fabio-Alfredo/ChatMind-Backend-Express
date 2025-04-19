6dbb8fd365a2ba8c38f186c49f0f29d75e96ac9e
Fabio-Alfredo <00210121@uca.edu.sv>
2025-04-18 19:22:17 -0600

    add(user.repository): add new function for update roles in user :sparkles:

--- src/repositories/user.repository.ts
+++ src/repositories/user.repository.ts
@@ -1,6 +1,7 @@
 import UserModel from "../domains/models/user.model";
 import { RegisterGoogleUser, RegisterUser, User } from "../interfaces";
 import { Schema } from 'mongoose';
+import { Roles } from "../types/roles.types";
 
 export const create = async (user: RegisterUser | RegisterGoogleUser): Promise<User> => {
     const newUser = await UserModel.create(user);
@@ -20,5 +21,10 @@ export const findAll = async (): Promise<User[]> => {
 
 export const findByEmail = async (email: string): Promise<User | null> => {
     const user = await UserModel.findOne({ email });
+    return user;
+}
+
+export const updateRole = async (id: Schema.Types.ObjectId, role: Roles): Promise<User | null> => {
+    const user = await UserModel.findByIdAndUpdate(id, { role }, { new: true });
     return user;
 }
